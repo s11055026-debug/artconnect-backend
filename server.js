9fa449c88d06c9644ca6066d2c4bec66ca5f2ae9
s11055026-debug <s11055026@gm2.nutn.edu.tw>
2026-05-27 13:55:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json()); // 讓後端看得懂前端傳來的 JSON 資料
 
 // ⚠️ 請換成你們 Clever Cloud 網頁後台給的真實連線資訊！
 const db = mysql.createConnection({
-    host: '填入你的 Clever Cloud Host 網址',
+    host: 'b3jdnhv6auslxuvpf2mg-mysql.services.clever-cloud.com',
     user: 'umpzsf30yeeffpy', // 根據你的截圖，你的 User 固定是這個
     password: '填入你的 Clever Cloud Password 密碼',
     database: '填入你在上一輪滑鼠點兩下、b開頭的那個真實資料庫名稱', 
